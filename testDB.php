01d2112034355b848e7a9ae9d108ba19626ec63d
suemura <malus@swwsc.net>
2012-05-31 00:02:29 +0900

    change message

---
+++ testDB.php
@@ -0,0 +1,25 @@
+<?php
+	echo "qqq";
+    /*
+     ini_set( 'display_errors', 1 );
+     echo "ooo";
+     $file_name = "swwsc-post-pic.sqlite3";
+     if (! $db = new PDO($file_name)) {
+     die("DB Connection Failed.");
+     }
+     echo "cccc";
+     
+     $q = sprintf('insert into %s values(key="%s", name="%s", count="0")', $file_name, '001', 'namae desu');
+     if (! $db->query($q)) {
+     die("DB Insert Error.");
+     }
+     echo "dddd";
+     
+     $q = sprintf('select * from %s where key="001"', $file_name);
+     if (! $db->query($q)) {
+     die("DB Select Error.");
+     }
+     echo "eee";
+     */
+    
+    ?>
